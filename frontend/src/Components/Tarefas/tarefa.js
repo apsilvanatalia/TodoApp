a65aa07d3c3758dd6e8a7af167300e16f022917f
apsilvanatalia <apsilva.natalia@gmail.com>
2023-08-27 20:27:36 -0300

    frontend

---
+++ frontend/src/Components/Tarefas/tarefa.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Tarefas(){
+    return(
+        <>
+          <li className="tarefa-info">
+            <div>
+              <strong>Aprender React</strong>
+              <div>
+                x
+              </div>
+            </div>  
+            <textarea defaultValue="Lorem Ipsum is simply dummy text of the printing and typesetting industry."></textarea> 
+            <span>!</span>
+          </li>
+
+          
+        </>
+    )
+}
+
+export default Tarefas
