786915a755d3bece40672aaee73405ba7e01c5a0
apsilvanatalia <apsilva.natalia@gmail.com>
2023-09-12 19:24:43 -0300

    merge do backend

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://user-admin:T7wfU1YMT53hryLT@cluster0.nfywrnu.mongodb.net/TodoApp');
-//mongoose.connect('mongodb+srv://Enzovp01:Teste123@cluster0.kvxkpcl.mongodb.net/test');
+//mongoose.connect('mongodb+srv://user-admin:T7wfU1YMT53hryLT@cluster0.nfywrnu.mongodb.net/TodoApp');
+mongoose.connect('mongodb+srv://Enzovp01:Teste123@cluster0.kvxkpcl.mongodb.net/test');
 
 let db = mongoose.connection;
 
--- backend/src/models/TarefasDB.js
+++ backend/src/models/TarefasDB.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 
-const TarefaSchema = new mongoose.Schema({
+/*const TarefaSchema = new mongoose.Schema({
     title: String,
     tarefa: String,
     start: Date,
     conclusion: Date,
     status: Boolean,
     userId: String
-});
-/*
+});*/
+
 const TarefaSchema = new mongoose.Schema({
     title: String,
     description: String,
     conclusion: Date,
     status: String,
     userId: String,
-});*/
+});
 
-module.exports = mongoose.model('tarefas', TarefaSchema);
+//module.exports = mongoose.model('tarefas', TarefaSchema);
+module.exports = mongoose.model('tasks', TarefaSchema);
